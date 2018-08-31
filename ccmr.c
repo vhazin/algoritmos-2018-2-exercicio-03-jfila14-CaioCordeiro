993e875492cfb0546066b7b6d66d83f65415a63c
CaioCordeiro <37188299+CaioCordeiro@users.noreply.github.com>
2018-08-31 10:04:45 -0300

    Update ccmr.c
    agr ele ta bonito

--- ccmr.c
+++ ccmr.c
@@ -1,107 +1,117 @@
 #include <stdio.h>
 #include <malloc.h>
 
-struct elemento {
-        struct elemento *proximo;
-        int info;
-
+struct elemento
+{
+    struct elemento *proximo;
+    int info;
 };
 
-struct Lista {
+struct Lista
+{
     struct elemento *dados;
     int tamanho;
 };
 
-
-struct Lista * criarLista(){
+struct Lista *criarLista()
+{
     struct Lista *aLista;
     aLista = malloc(sizeof(struct Lista));
-    if(aLista != NULL){
+    if (aLista != NULL)
+    {
         aLista->tamanho = 0;
         aLista->dados = NULL;
     }
     return aLista;
-
 }
-int adicionarnaposicao(struct Lista *aLista, int *info,int posicao){
-    struct elemento *novo,*anterior;
-    if(posicao>aLista->tamanho +1){
-        return(printf("Error(Posi)"));
-
+int adicionarnaposicao(struct Lista *aLista, int *info, int posicao)
+{
+    struct elemento *novo, *anterior;
+    if (posicao > aLista->tamanho + 1)
+    {
+        return (printf("Error(Posi)"));
     }
-    else{
+    else
+    {
         novo = malloc(sizeof(struct elemento));
-        if(posicao == 1){
+        if (posicao == 1)
+        {
             aLista->tamanho = aLista->tamanho + 1;
             novo->info = info;
-            novo->proximo =aLista->dados;
+            novo->proximo = aLista->dados;
             aLista->dados = novo;
-            return(aLista->tamanho);
+            return (aLista->tamanho);
         }
-        else{
-            if(novo == NULL){
-                return(printf("Error(Full)"));
+        else
+        {
+            if (novo == NULL)
+            {
+                return (printf("Error(Full)"));
             }
-            else{
+            else
+            {
                 anterior = aLista->dados;
-                for(int i = 0;(posicao-2);i++){
+                for (int i = 0; (posicao - 2); i++)
+                {
                     anterior = anterior->proximo;
                 }
                 novo->proximo = anterior->proximo;
                 novo->info = info;
                 anterior->proximo = novo;
-                aLista->tamanho = aLista ->tamanho +1;
-                return(aLista->tamanho);
+                aLista->tamanho = aLista->tamanho + 1;
+                return (aLista->tamanho);
             }
-
         }
     }
-
 }
-int retira(struct Lista *aLista,int valor){
-    struct elemento *anterior,*auxiliar;
+int retira(struct Lista *aLista, int valor)
+{
+    struct elemento *anterior, *auxiliar;
     int *volta;
-    auxiliar= aLista->dados;
-    while(auxiliar != NULL){
-        if(auxiliar->info==valor){
-            if(anterior==NULL){
-                aLista->dados=auxiliar->proximo;
+    auxiliar = aLista->dados;
+    while (auxiliar != NULL)
+    {
+        if (auxiliar->info == valor)
+        {
+            if (anterior == NULL)
+            {
+                aLista->dados = auxiliar->proximo;
             }
-            else{
-                anterior->proximo=auxiliar->proximo;
+            else
+            {
+                anterior->proximo = auxiliar->proximo;
             }
-            aLista->tamanho = aLista->tamanho - 1;    
+            aLista->tamanho = aLista->tamanho - 1;
             return 1;
         }
         anterior = auxiliar;
         auxiliar = auxiliar->proximo;
     }
-
 }
 /*int exibe(struct Lista *aLista){
     struct elemento *aux;
     aux = aLista->
 
 }*/
-int main(){
+int main()
+{
     int pinline;
     int poutline;
     int temp1;
     int temp2;
     struct Lista *aLista;
 
-    
-    scanf("%n",pinline);
-    aLista =criarLista();
-    for(int i = 0;i==pinline;i++){
-        scanf("%d",&temp1);
-        adicionarnaposicao(aLista, temp1,i );
-        }    
-    scanf("%d",&poutline);
-    for(int a = 0;a==poutline;a++){
-        scanf("%d",&temp2);
-        retira(aLista,temp2);
-
-        }
-    
+    scanf("%n", pinline);
+    aLista = criarLista();
+    for (int i = 0; i == pinline; i++)
+    {
+        scanf("%d", &temp1);
+        adicionarnaposicao(aLista, temp1, i);
     }
+    scanf("%d", &poutline);
+    for (int a = 0; a == poutline; a++)
+    {
+        scanf("%d", &temp2);
+        retira(aLista, temp2);
+    }
+}
